9549bc92b159c363207250bf2fa236d9b1b7bc6e
JKostov <julijekos@gmail.com>
2020-01-18 13:49:48 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Spark.Sql;
+
+namespace SparkClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataPath = "data-to-process/traffic-data.csv";
+            var Spark = SparkSession
+                           .Builder()
+                           .GetOrCreate();
+            var dataFrame = Spark.Read().Csv(dataPath);
+            dataFrame.PrintSchema();
+            dataFrame.Show();
+        }
+    }
+}
---
+++ data-to-process/data.md
@@ -0,0 +1,7 @@
+﻿# Data to process
+
+## 1. [Download data](https://osu.app.box.com/v/lstw-traffic-weather-v2)
+
+## 2. Extract here
+
+## 3. Rename the extracted file to `traffic-data`
